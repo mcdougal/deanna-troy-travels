afda76cc0415c3df45423ecbe6ad5eab8ce9a747
Cedric McDougal <cedric@neighborschools.com>
2022-07-03 15:35:02 -0400

    Use Emotion in SSR

--- src/containers/Document/Document.tsx
+++ src/containers/Document/Document.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @next/next/no-document-import-in-page */
-import createEmotionServer from '@emotion/server/types/create-instance';
+import createEmotionServer from '@emotion/server/create-instance';
 import { AppType } from 'next/dist/shared/lib/utils';
 import NextDocument, {
   DocumentContext,
---
+++ src/pages/_document.ts
@@ -0,0 +1,5 @@
+/* eslint-disable filenames/match-exported */
+/* eslint-disable filenames/match-regex */
+import Document from 'src/containers/Document';
+
+export default Document;
