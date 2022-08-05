15ec74e99a37d4cecc041506c39e88aec559ec4d
Cedric McDougal <cedric@neighborschools.com>
2022-08-05 16:00:40 -0400

    temp

--- src/pageComponents/HomePage/HeroImage/HeroImage.styles.ts
+++ src/pageComponents/HomePage/HeroImage/HeroImage.styles.ts
@@ -2,18 +2,12 @@ import { spacing } from '@lib/mui';
 
 export default {
   heroSectionContainer: {
+    position: `relative`,
+  },
+  circleAndImageContainer: {
     height: spacing(50),
     overflow: `hidden`,
     position: `relative`,
-  },
-  heroImageContainer: {
-    bottom: spacing(-1),
-    height: spacing(45),
-    left: `50%`,
-    maxWidth: spacing(100),
-    position: `absolute`,
-    transform: `translateX(-50%)`,
-    width: `100%`,
   },
   circleBackground: {
     backgroundColor: `#f9f9f9`,
@@ -25,5 +19,53 @@ export default {
     right: { xs: `-75vw`, md: `-5vw` },
     transform: `translateY(55%)`,
     width: { xs: `250vw`, md: `110vw` },
+  },
+  heroImageContainer: {
+    bottom: spacing(-1),
+    height: spacing(45),
+    left: `50%`,
+    maxWidth: spacing(100),
+    position: `absolute`,
+    transform: `translateX(-50%)`,
+    width: `100%`,
+  },
+  emoji: {
+    alignItems: `center`,
+    background: `#f9f9f9`,
+    borderRadius: `100%`,
+    display: { xs: `none`, lg: `flex` },
+    fontSize: 20,
+    height: spacing(6),
+    justifyContent: `center`,
+    position: `absolute`,
+    width: spacing(6),
+  },
+  emoji1: {
+    left: 100,
+    top: 100,
+  },
+  emoji2: {
+    right: 100,
+    top: 50,
+  },
+  emoji3: {
+    left: 250,
+    top: -50,
+  },
+  emoji4: {
+    right: 300,
+    top: -100,
+  },
+  emoji5: {
+    left: 75,
+    top: -175,
+  },
+  emoji6: {
+    left: 350,
+    top: -215,
+  },
+  emoji7: {
+    right: 50,
+    top: -175,
   },
 };
--- src/pageComponents/HomePage/HeroImage/HeroImage.tsx
+++ src/pageComponents/HomePage/HeroImage/HeroImage.tsx
@@ -4,19 +4,30 @@ import Image from 'next/image';
 import sx from './HeroImage.styles';
 
 const HeroImage = (): JSX.Element => {
+  const emojis = [`😎`, `✈️`, `🏝️`, `⛰️`, `🏖️`, `🗽`, `⛴️`];
+
   return (
     <Box sx={sx.heroSectionContainer}>
-      <Box sx={sx.circleBackground} />
-      <Box sx={sx.heroImageContainer}>
-        <Image
-          alt="Three photos: Deanna lying on a sand dune, Deanna doing a yoga pose on a beach, and Deanna in front of a mosque in Malaysia"
-          layout="fill"
-          objectFit="cover"
-          objectPosition="top"
-          priority
-          src="/deanna-troy-travels/home/hero.png"
-        />
+      <Box sx={sx.circleAndImageContainer}>
+        <Box sx={sx.circleBackground} />
+        <Box sx={sx.heroImageContainer}>
+          <Image
+            alt="Three photos: Deanna lying on a sand dune, Deanna doing a yoga pose on a beach, and Deanna in front of a mosque in Malaysia"
+            layout="fill"
+            objectFit="cover"
+            objectPosition="top"
+            priority
+            src="/deanna-troy-travels/home/hero.png"
+          />
+        </Box>
       </Box>
+      {emojis.map((emoji, i) => {
+        return (
+          <Box key={emoji} sx={[sx.emoji, sx[`emoji${i + 1}`]]}>
+            {emoji}
+          </Box>
+        );
+      })}
     </Box>
   );
 };
