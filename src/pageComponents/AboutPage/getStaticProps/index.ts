cb89bbe9adccab814e7e991a4517a268a6cb573f
Cedric <mcdougal.ce@gmail.com>
2024-11-16 05:10:25 -0500

    tgemp

--- src/pageComponents/AboutPage/getStaticProps/index.ts
+++ src/pageComponents/AboutPage/getStaticProps/index.ts
@@ -1,8 +1,3 @@
-import { BlogPost } from './fetchRecentBlogPosts';
-import { InstagramPost } from './fetchRecentInstagramPosts';
-import { YouTubeVideo } from './fetchRecentVideos';
 import getStaticProps from './getStaticProps';
-
-export type { BlogPost, InstagramPost, YouTubeVideo };
 
 export default getStaticProps;
