a717faf0c5fc2f60f9212e07b16618436eecd453
Cedric <mcdougal.ce@gmail.com>
2026-01-03 14:27:06 -0500

    Fix contact dialog submission bug

--- src/components/site/ContactDialog/ContactDialog.tsx
+++ src/components/site/ContactDialog/ContactDialog.tsx
@@ -4,11 +4,13 @@ import {
   Dialog,
   DialogActions,
   DialogContent,
+  DialogContentText,
   DialogTitle,
   TextField,
   useMediaQuery,
   useTheme,
 } from '@mui/material';
+import { useEffect, useState } from 'react';
 
 type Props = {
   onClose: () => void;
@@ -18,9 +20,54 @@ type Props = {
 const ContactDialog = ({ onClose, open }: Props): JSX.Element => {
   const theme = useTheme();
   const isFullScreen = useMediaQuery(theme.breakpoints.down(`sm`));
+  const [successOpen, setSuccessOpen] = useState(false);
+
+  const handleFormSubmit = async (
+    event: React.FormEvent<HTMLFormElement>,
+  ): Promise<void> => {
+    event.preventDefault();
+    const target = event.target as HTMLFormElement;
+    const formData = new FormData(target);
+    await fetch(`/__forms.html`, {
+      method: `POST`,
+      headers: { 'Content-Type': `application/x-www-form-urlencoded` },
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      body: new URLSearchParams(formData as any).toString(),
+    });
+    target.reset();
+    setSuccessOpen(true);
+  };
+
+  useEffect(() => {
+    if (!open) {
+      setSuccessOpen(false);
+    }
+  }, [open]);
+
+  if (open && successOpen) {
+    return (
+      <Dialog
+        onClose={(): void => {
+          return setSuccessOpen(false);
+        }}
+        open={successOpen}>
+        <DialogTitle>Message Sent!</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            I’ll reply to your message shortly. Keep an eye on your inbox!
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button color="inherit" onClick={onClose} size="large" variant="text">
+            Close
+          </Button>
+        </DialogActions>
+      </Dialog>
+    );
+  }
 
   return (
-    <form action="/success" data-netlify="true" method="POST" name="contact">
+    <form name="contact" onSubmit={handleFormSubmit}>
       <Dialog
         disablePortal
         fullScreen={isFullScreen}
